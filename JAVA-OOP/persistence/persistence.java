a8d5237e348f318cc38253fe483b33d233723660
ayesha8786 <97177422+ayesha8786@users.noreply.github.com>
2022-01-14 20:45:06 +0530

    Add files via upload

---
+++ JAVA-OOP/persistence/persistence.java
@@ -0,0 +1,19 @@
+package persistent;
+
+public abstract class persistence {
+	public abstract void persist();
+}
+
+class filepersistence extends persistence {
+	public void persist() {
+		System.out.println("the data stored in files");
+
+	}
+}
+
+class datapersistence extends filepersistence {
+	public void persist() {
+		System.out.println("the data stored in database");
+
+	}
+}
