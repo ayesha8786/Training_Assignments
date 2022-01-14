3eb9862e16454d0db997b6d2515102ef4cbfc0c8
ayesha8786 <97177422+ayesha8786@users.noreply.github.com>
2022-01-14 21:13:51 +0530

    Add files via upload

---
+++ Java_Strings/string_concat.java
@@ -0,0 +1,13 @@
+package strings;
+
+public class string_concat {
+		public static void main(String[] args)
+		{
+			String str1="Hello,";
+			String str2="How are you";
+			String con=str1.concat(str2);
+			System.out.println(con);
+		}
+
+	}
+
---
+++ Java_Strings/stringbufferinsert.java
@@ -0,0 +1,14 @@
+package strings;
+
+import java.lang.String;
+
+public class stringbufferinsert {
+
+	public static void main(String[] args) {
+		StringBuffer insert = new StringBuffer();
+		insert.insert(0, "insert text into a  string");
+		insert.insert(16, "_");
+		System.out.println(insert);
+	}
+
+}
