220567667472bb058c5362b04fe48cc82c93a53d
ayesha8786 <97177422+ayesha8786@users.noreply.github.com>
2022-01-19 17:13:34 +0530

    Add files via upload

---
+++ Java_Generics/pair.java
@@ -0,0 +1,13 @@
+package generics;
+
+import java.util.*;
+
+public class pair {
+	public static void main(String[] args) {
+		Map<String, String> map = new HashMap<>();
+		map.put("Hello", "6");
+		map.put("Welcome", "10");
+		System.out.println(map);
+
+	}
+}
