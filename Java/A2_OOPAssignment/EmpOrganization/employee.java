740a66a6b3ee3be56488f37961f900ec756e54c7
ayesha8786 <97177422+ayesha8786@users.noreply.github.com>
2022-01-20 19:29:05 +0530

    Add files via upload

---
+++ Java/A2_OOPAssignment/EmpOrganization/employee.java
@@ -0,0 +1,18 @@
+package EmpOrganization;
+
+import java.lang.*;
+class employee
+{
+	String n;
+	int sal;
+	employee(String n,int sal)
+	{
+		this.n=n;
+		this.sal=sal;
+		
+	}
+	int getsalary()
+	{
+		return sal;
+	}
+}
---
+++ Java/A2_OOPAssignment/EmpOrganization/manager.java
@@ -0,0 +1,16 @@
+package EmpOrganization;
+
+public class manager extends employee {
+	int hourly,incentives;
+	manager(String n,int sal,int i)
+	{
+		super(n,sal);
+		incentives=i;
+		
+		
+	}
+	int getsalary() {
+		return(super.getsalary()+incentives);
+	}
+
+}
